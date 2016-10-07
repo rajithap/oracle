432f6388b9f743f716e813b7d6fd2a0c1afdbcf7
rajithap <smilerkid@yahoo.com>
2016-10-07 14:39:22 -0400

    stuff

--- actions/botactions.js
+++ actions/botactions.js
@@ -26,7 +26,7 @@
                  case 'add':
                     sum = 0
                     for (i=0; i < entities.length; i++){
-                        if (entities[i].type == "builtin.number"):
+                        if (entities[i].type == "builtin.number")
                             sum+= entities[i].entity;
                     }
 
