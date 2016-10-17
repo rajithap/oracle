d696967d5e2d973f757a9f283f4ad4d097441d9b
rajithap <smilerkid@yahoo.com>
2016-10-17 15:01:36 -0400

    starting division

--- actions/botactions.js
+++ actions/botactions.js
@@ -24,7 +24,7 @@
                     actionResult.spokenText = "Well hello there how are you!";
                     break;
                  case 'add':
-                    sum = 0
+                    sum = 0;
                     if (entities.length == 1 && entities[0].type == "builtin.number"){
                         sum = parseInt(entities[0].entity)*2;
                     }
@@ -39,16 +39,23 @@
                     actionResult.spokenText = actionResult.displayText;
                     break;
                 case 'subtract':
+                    for (i=0; i<entities.length; i++){
+                        if (entities[0].type == "builtin.number")
+                            diff = parseInt(entities[i].entity);
+                            start = i
+                            break;
+                    }
+
                     if (entities.length == 1 && entities[0].type == "builtin.number"){
                         txt = entities[0].entity;
                         nums = txt.split(' - ');
                         diff = parseInt(nums[0]);
-                        for (i = 1; i< nums.length; i++){
+                        for (i = start; i< nums.length; i++){
                             diff -= parseInt(nums[i]);
                         }
                     }
                     else{
-                        diff = parseInt(entities[0].entity);
+                        if (entities[0].type == "builtin.number") diff = parseInt(entities[0].entity);
                         for (i=1; i < entities.length; i++){
                             if (entities[i].type == "builtin.number")
                                 diff-= parseInt(entities[i].entity);
@@ -58,9 +65,35 @@
                     actionResult.displayText = "The difference is " + diff;
                     actionResult.spokenText = actionResult.displayText;
                     break;
+                case 'multiply':
+                    product = 1;
+                    for (i=1; i<entities.length; i++){
+                        if (entities[i].type == "builtin.number")
+                                product*= parseInt(entities[i].entity);
+                    }
+
+                    actionResult.displayText = "The product is " + product;
+                    actionResult.spokenText = actionResult.displayText;
+                    break;
+                case 'divide':
+                    for (i=0; i<entities.length; i++){
+                        if (entities[0].type == "builtin.number")
+                            quot = parseInt(entities[i].entity);
+                            start = i
+                            break;
+                    }
+                    
+                    for (i=start+1; i<entities.length; i++){
+                        if (entities[i].type == "builtin.number")
+                                quot /= parseInt(entities[i].entity);
+                    }
+
+                    actionResult.displayText = "The quotient is " + quot;
+                    actionResult.spokenText = actionResult.displayText;
+                    break;
                 case 'greetings':
-                    actionResult.displayText = "Well, hello there!";
-                    actionResult.spokenText = "Well hello there!";
+                    actionResult.displayText = "Hi! Nice to meet you!";
+                    actionResult.spokenText = actionResult.displayText;
                     break;
                 case 'tellname':
                     actionResult.displayText = "My Name is R";
@@ -77,7 +110,7 @@
                     });                    
                     break;
                 default:
-                    actionResult.displayText = "Sorry I am not yet capable of handling "+intentResult;
+                    actionResult.displayText = "Sorry I am not yet capable of handling that.";
                     actionResult.spokenText = actionResult.displayText
             }   
             return actionResult;
